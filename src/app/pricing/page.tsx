812d57ed3a11bbe2cd08de8d3a64886957dfe7f3
Josh Murtha <50592837+murthajosh@users.noreply.github.com>
2026-04-28 14:44:21 -0500

    layout and content

--- src/app/pricing/page.tsx
+++ src/app/pricing/page.tsx
@@ -24,8 +24,8 @@ export default function Pricing() {
         <section className="rounded-xl p-6 flex flex-col justify-start items-center gap-8 mx-auto w-full max-w-4xl">
             <h2 className="font-bold text-3xl">Real Pricing Examples</h2>
 
-            <div className="flex flex-col gap-6 md:w-full md:flex-row md:justify-between">
-                <div className="flex flex-col gap-3">
+            <div className="flex flex-col gap-6 w-full md:flex-row md:justify-between">
+                <div className="border-2 rounded-xl w-full p-3 flex flex-col gap-3">
                     <h3 className="font-bold text-xl text-">Classic Blues Trio</h3>
                     <ul className="">
                         <li>(3 musicians x $200 = $600)</li>
@@ -34,7 +34,7 @@ export default function Pricing() {
                     <p className="font-bold">Total: $1,200</p>
                 </div>
 
-                <div className="flex flex-col gap-3">
+                <div className="border-2 rounded-xl w-full p-3 flex flex-col gap-3">
                     <h3 className="font-bold text-xl text-">Rockin&apos; Blues Quartet</h3>
                     <ul className="">
                         <li>(4 musicians x $200 = $800)</li>
@@ -43,7 +43,7 @@ export default function Pricing() {
                     <p className="font-bold">Total: $2,400</p>
                 </div>
 
-                <div className="flex flex-col gap-3">
+                <div className="border-2 rounded-xl w-full p-3 flex flex-col gap-3">
                     <h3 className="font-bold text-xl text-">5-Piece Blues Band</h3>
                     <ul className="">
                         <li>(5 musicians x $200 = $1,000)</li>
@@ -56,24 +56,26 @@ export default function Pricing() {
         </section>
 
         <section className="rounded-xl p-6 flex flex-col justify-start gap-8 mx-auto w-full max-w-4xl">
-            <h2 className="font-bold text-3xl">What&apos;s Included</h2>
+            <h2 className="font-bold text-3xl">Hassle-free, guaranteed.</h2>
 
-            <div className="flex flex-col gap-3">
-                <h3 className="font-bold text-xl">Included with every booking</h3>
-                <ul className="flex flex-col gap-3">
-                    <li className="flex gap-1 items-center"><CheckCircle className="text-green-500" size={16}/>Pro Chicago Blues musicians who look and sound great</li>
-                    <li className="flex gap-1 items-center"><CheckCircle className="text-green-500" size={16}/>An authentic soundtrack to your event</li>
-                    <li className="flex gap-1 items-center"><CheckCircle className="text-green-500" size={16}/>Fast communication and flexibility</li>
-                </ul>
-            </div>
+            <div className="grid grid-cols-1 gap-6 md:grid-cols-2">
+                <div className="flex flex-col gap-3">
+                    <h3 className="font-bold text-xl">Included with every booking</h3>
+                    <ul className="flex flex-col gap-3">
+                        <li className="flex gap-1 items-center"><CheckCircle className="text-green-500" size={16}/>Pro Chicago Blues musicians</li>
+                        <li className="flex gap-1 items-center"><CheckCircle className="text-green-500" size={16}/>An authentic soundtrack to your event</li>
+                        <li className="flex gap-1 items-center"><CheckCircle className="text-green-500" size={16}/>Fast communication and flexibility</li>
+                    </ul>
+                </div>
 
-            <div className="flex flex-col gap-3">
-                <h3 className="font-bold text-xl">You DON&apos;T need to worry about:</h3>
-                <ul className="flex flex-col gap-3">
-                    <li className="flex gap-1 items-center"><XIcon className="text-red-500" size={16}/>Hidden fees</li>
-                    <li className="flex gap-1 items-center"><XIcon className="text-red-500" size={16}/>Feeding the band</li>
-                    <li className="flex gap-1 items-center"><XIcon className="text-red-500" size={16}/>Validating parking</li>
-                </ul>
+                <div className="flex flex-col gap-3">
+                    <h3 className="font-bold text-xl">You DON&apos;T need to worry about</h3>
+                    <ul className="flex flex-col gap-3">
+                        <li className="flex gap-1 items-center"><XIcon className="text-red-500" size={16}/>Hidden fees</li>
+                        <li className="flex gap-1 items-center"><XIcon className="text-red-500" size={16}/>Feeding the band</li>
+                        <li className="flex gap-1 items-center"><XIcon className="text-red-500" size={16}/>Validating parking</li>
+                    </ul>
+                </div>
             </div>
         </section>
 

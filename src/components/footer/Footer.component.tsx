1a9e806d088312ad02bf67ef69ed6cbbe9dae6c0
Josh Murtha <50592837+murthajosh@users.noreply.github.com>
2026-04-28 13:14:32 -0500

    typo

--- src/components/footer/Footer.component.tsx
+++ src/components/footer/Footer.component.tsx
@@ -7,7 +7,7 @@ const footerLinks = [
 const Footer = () => {
   return (
     <footer className="w-full border-t px-4 py-4 text-center text-sm text-gray-500">
-      <div>© {new Date().getFullYear()} ChicagoBlesBands.com</div>
+      <div>© {new Date().getFullYear()} ChicagoBluesBands.com</div>
       <div className="mt-2 flex flex-wrap gap-4 justify-center">
         {footerLinks.map((link) => (
           <Link key={link.href} href={link.href} className="hover:underline">
